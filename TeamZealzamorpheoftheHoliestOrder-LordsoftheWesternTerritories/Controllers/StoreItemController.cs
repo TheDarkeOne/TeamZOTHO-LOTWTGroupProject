9b9c8fe44110b7f9716ffc8162de096c13ac48ad
Caleb Morales <clbmrls@gmail.com>
2021-01-30 14:39:32 -0700

    added CRUD to StoreItem controller

--- TeamZealzamorpheoftheHoliestOrder-LordsoftheWesternTerritories/Controllers/StoreItemController.cs
+++ TeamZealzamorpheoftheHoliestOrder-LordsoftheWesternTerritories/Controllers/StoreItemController.cs
@@ -16,13 +16,58 @@ namespace TeamZealzamorpheoftheHoliestOrder_LordsoftheWesternTerritories.Control
     {
 
         private readonly IDataService dataService;
-
         public StoreItemController(IDataService dataService)
         {
             this.dataService = dataService ?? throw new ArgumentNullException(nameof(dataService));
         }
 
-        [HttpGet()]
-        public async Task<List<StoreItem>> Get() => await dataService.Items.ToListAsync();
+        [HttpGet("[action]")]
+        public async Task<List<StoreItem>> GetItems() => await dataService.Items.ToListAsync();
+        [HttpGet("[action]")]
+        public async Task<List<Category>> GetCategories() => await dataService.Categories.ToListAsync();
+
+        [HttpGet("[action]")]
+        public async Task<StoreItem> GetItemById(int id) 
+        {
+            return await dataService.Items.Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        [HttpGet("[action]")]
+        public async Task<Category> GetCategoryById(int id)
+        {
+            return await dataService.Categories.Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        [HttpPost("[action]")]
+        public async Task AddCategory(Category category)
+        {
+            await dataService.CreateCategory(category);
+        }
+        [HttpPost("[action]")]
+        public async Task AddItem(StoreItem item)
+        {
+            await dataService.CreateItem(item);
+        }
+        [HttpPost("[action]")]
+        public async Task UpdateItem(StoreItem item)
+        {
+            await dataService.UpdateItem(item);
+        }
+        [HttpPost("[action]")]
+        public async Task UpdateCategory(Category category)
+        {
+            await dataService.UpdateCategory(category);
+        }
+        [HttpPost("[action]")]
+        public async Task DeleteItem(StoreItem item)
+        {
+            await dataService.DeleteItem(item);
+        }
+
+        [HttpPost("[action]")]
+        public async Task DeleteCategory(Category category)
+        {
+            await dataService.DeleteCategory(category);
+        }
     }
 }
