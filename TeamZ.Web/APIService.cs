5d9cee43cebd99b615125baebb29097cf21d17bf
Caleb Morales <clbmrls@gmail.com>
2021-02-13 16:07:24 -0700

    Added the front-end that simply displays our list of store items and teases the construct-a-croc feature

---
+++ TeamZ.Web/APIService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using TeamZ.Shared;
+
+namespace TeamZ.Web
+{
+    public class APIService
+    {
+        private readonly HttpClient client;
+        public APIService(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<List<StoreItem>> GetStoreItemsAsync()
+        {
+            return await client.GetFromJsonAsync<List<StoreItem>>("api/storeitem/getitems");
+        }
+
+    }
+}
