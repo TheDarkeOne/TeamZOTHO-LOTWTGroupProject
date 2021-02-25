03d4b963819cdf8b872fe49dd9c8efa343ff3f65
Caleb Morales <clbmrls@gmail.com>
2021-02-24 20:55:25 -0700

    Got a construct-a-croc page working, and we learned about EditForm and models

---
+++ TeamZ.Web/FormModels/CrocModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamZ.Web.FormModels
+{
+    public class CrocModel
+    {
+        [Required]
+        [StringLength(15, ErrorMessage = "Color too long.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Characters are not allowed.")]
+        public string Color { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "Hobby too long.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Characters are not allowed.")]
+        public string Hobby { get; set; }
+
+        [StringLength(20, ErrorMessage = "Hat too long.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Characters are not allowed.")]
+        public string Hat { get; set; }
+
+        [StringLength(20, ErrorMessage = "Tail too long.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Characters are not allowed.")]
+        public string Tail { get; set; }
+
+        [StringLength(20, ErrorMessage = "Held Item too long.")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Characters are not allowed.")]
+        public string HeldItem { get; set; }
+    }
+}
