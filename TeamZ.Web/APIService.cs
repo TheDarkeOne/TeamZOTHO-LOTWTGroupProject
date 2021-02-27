73def607a72aa10a1078f2613134bc403fa7c7a8
Caleb Morales <clbmrls@gmail.com>
2021-02-27 14:32:18 -0700

    Added the Result Class for error handling in Blazor

--- TeamZ.Web/APIService.cs
+++ TeamZ.Web/APIService.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using TeamZ.Shared;
+using TeamZ.Web.FormModels;
 
 namespace TeamZ.Web
 {
@@ -16,9 +17,18 @@ namespace TeamZ.Web
             this.client = client;
         }
 
-        public async Task<List<StoreItem>> GetStoreItemsAsync()
+        public async Task<Result<List<StoreItem>>> GetStoreItemsAsync()
         {
-            return await client.GetFromJsonAsync<List<StoreItem>>("api/storeitem/getitems");
+            try
+            {
+                var result = await client.GetFromJsonAsync<List<StoreItem>>("api/storeitem/getitems");
+                return result is null ? Result.Fail<List<StoreItem>>("No Items Found.") : Result.Ok(result);
+
+            }
+            catch (Exception e)
+            {
+                return Result.Fail<List<StoreItem>>("Error while getting Store Items: " + e.Message);
+            }
         }
 
         public async Task PostConstructACrocAsync(string color, string hobby, string hat = "", string tail = "", string heldItem = "")
@@ -35,9 +45,17 @@ namespace TeamZ.Web
             await client.PostAsJsonAsync("api/storeitem/constructacroc", croc);
         }
 
-        public async Task<StoreItem> GetStoreItemById(int id)
+        public async Task<Result<StoreItem>> GetStoreItemById(int id)
         {
-            return await client.GetFromJsonAsync<StoreItem>($"api/storeitem/getitembyid/?id={id}");
+            try
+            {
+                var result = await client.GetFromJsonAsync<StoreItem>($"api/storeitem/getitembyid/?id={id}");
+                return result is null ? Result.Fail<StoreItem>("No item found with this id.") : Result.Ok(result);
+            } catch (Exception e)
+            {
+                return Result.Fail<StoreItem>("Error while getting Store Item:" + e.Message);
+            }
+            //return await client.GetFromJsonAsync<StoreItem>($"api/storeitem/getitembyid/?id={id}");
         }
 
     }
---
+++ TeamZ.Web/Result.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamZ.Web.FormModels
+{
+    public class Result
+    {
+        public bool Success { get; }
+        public string Error { get; private set; }
+        public bool IsFailure => !Success;
+
+        protected Result(bool success, string error)
+        {
+            if (success && error != string.Empty)
+                throw new InvalidOperationException();
+            if (!success && error == string.Empty)
+                throw new InvalidOperationException();
+            Success = success;
+            Error = error;
+        }
+
+        public static Result Fail(string message) => new Result(false, message);
+        public static Result<T> Fail<T>(string message) => new Result<T>(default(T), false, message);
+        public static Result Ok() => new Result(true, string.Empty);
+        public static Result<T> Ok<T>(T value) => new Result<T>(value, true, string.Empty);
+    }
+
+    public class Result<T> : Result
+    {
+        public T Value { get; set; }
+
+        protected internal Result(T value, bool success, string error) : base(success, error)
+        {
+            Value = value;
+        }
+    }
+}
